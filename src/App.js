b7cbeb5a1c561046f6e2937e07ad8e65fc09487d
Bubumariia Kudaiberdieva <mariiakudaiberdieva@gmail.com>
2022-10-24 11:32:13 +0300

    solved errors referring to the path directory

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ import NotesList from "./components/Notes/NotesList";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleThemeMode } from "./redux/themeReducer";
 
-// 1. Create Search Reducer
-// 2. Create Note Reducer
-
 export default function App() {
   const darkMode = useSelector((state) => state.theme.darkMode);
   const notes = useSelector((state) => state.notes.notes);
